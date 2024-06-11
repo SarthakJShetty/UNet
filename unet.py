5b0b1b6bd7bf87151ee8bdad9272269c9f9c8c4a
Sarthak J Shetty <sarthakshetty97@gmail.com>
2024-06-11 17:59:27 -0400

    expansive branch works. next up are the skip connections. also added `.vscode` settings.

--- unet.py
+++ unet.py
@@ -20,10 +20,10 @@ class ContractingBranch(torch.nn.Module):
             )
             for i in range(0, len(out_channels) - 1)
         ]
-        self.blocks = torch.nn.Sequential(*self._blocks_list)
+        self._contracting_branch = torch.nn.Sequential(*self._blocks_list)
 
     def forward(self, image: torch.Tensor) -> torch.Tensor:
-        return self.blocks(image)
+        return self._contracting_branch(image)
 
 
 class BottleNeck(ContractingBranch):
@@ -42,8 +42,26 @@ class BottleNeck(ContractingBranch):
 
 
 class ExpandingBranch(torch.nn.Module):
-    def __init__(self):
+    def __init__(
+        self,
+        in_channels=[1024, 512, 256, 128, 64],
+    ):
         super().__init__()
+        self._blocks_list = [
+            torch.nn.Sequential(
+                torch.nn.ConvTranspose2d(in_channels=in_channels[i], out_channels=in_channels[i + 1], kernel_size=(2, 2), stride=2),
+                torch.nn.Conv2d(in_channels=in_channels[i + 1], out_channels=in_channels[i + 1], kernel_size=(3, 3)),
+                torch.nn.ReLU(),
+                torch.nn.Conv2d(in_channels=in_channels[i + 1], out_channels=in_channels[i + 1], kernel_size=(3, 3)),
+                torch.nn.ReLU(),
+            )
+            for i in range(len(in_channels) - 1)
+        ]
+        self._expanding_branch = torch.nn.Sequential(*self._blocks_list)
+
+    def forward(self, bottleneck_latent: torch.Tensor) -> torch.Tensor:
+        _output = self._expanding_branch(bottleneck_latent)
+        set_trace()
 
 
 class UNet(torch.nn.Module):
@@ -56,7 +74,7 @@ class UNet(torch.nn.Module):
     def forward(self, image: torch.Tensor) -> torch.Tensor:
         _contracting_block_latent = self._contracting_branch(image)
         _bottleneck_latent = self._bottleneck(_contracting_block_latent)
-        set_trace()
+        _expanding_block_output = self._expanding_branch(_bottleneck_latent)
 
 
 if __name__ == "__main__":
