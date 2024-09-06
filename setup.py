7a9a361840ec6b08fc178780a43ada8e27e3219e
Sarthak J Shetty <sarthakshetty97@gmail.com>
2024-09-06 17:01:10 -0400

    easier setup and installation

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
-from setuptools import setup
+from setuptools import find_packages, setup
+
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+parsed_requirements = [requirement in requirements if "--index-url" not in requirement]
 
 setup(
     name="unet",
     version="0.1",
-    description="simple unet architecture",
+    description="a simple unet implementation",
     author="sarthak",
     author_email="sarthakshetty97@gmail.com",
-    packages=["unet"],
-    install_requires=[
-        "matplotlib==3.9.0",
-        "tqdm",
-        "matplotlib-inline==0.1.7",
-    ],
+    packages=find_packages(),
+    install_requires=parsed_requirements,
 )
